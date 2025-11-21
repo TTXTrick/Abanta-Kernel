45bb1889693978289ec72aebd27fe6d18250aaab
TTXTrick <ttxtrick@gmail.com>
2025-11-21 16:20:11 -0500

    Update eficon.h

--- src/include/eficon.h
+++ src/include/eficon.h
@@ -1,11 +1,39 @@
-#ifndef EFICON_H
-#define EFICON_H
+#ifndef _EFI_CON_H_
+#define _EFI_CON_H_
 
-typedef struct {
-    EFI_STATUS (*Reset)(void *Self, BOOLEAN ExtendedVerification);
-    EFI_STATUS (*OutputString)(void *Self, CHAR16 *String);
-} EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+#include "efidef.h"
 
-// You can add more later if needed
+/*
+ * Simple Text Output Protocol
+ */
+
+typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+
+typedef EFI_STATUS (*EFI_TEXT_RESET)(
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
+    BOOLEAN ExtendedVerification
+);
+
+typedef EFI_STATUS (*EFI_TEXT_OUTPUT_STRING)(
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
+    CHAR16 *String
+);
+
+typedef EFI_STATUS (*EFI_TEXT_CLEAR_SCREEN)(
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This
+);
+
+struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
+    EFI_TEXT_RESET          Reset;
+    EFI_TEXT_OUTPUT_STRING  OutputString;
+    void                   *TestString;
+    void                   *QueryMode;
+    void                   *SetMode;
+    void                   *SetAttribute;
+    EFI_TEXT_CLEAR_SCREEN   ClearScreen;
+    void                   *SetCursorPosition;
+    void                   *EnableCursor;
+    void                   *Mode;
+};
 
 #endif

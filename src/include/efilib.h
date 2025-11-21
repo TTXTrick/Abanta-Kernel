6a0dc5552639f6e680a6e06aa14aec2a6909e205
TTXTrick <ttxtrick@gmail.com>
2025-11-21 16:20:27 -0500

    Update efilib.h

--- src/include/efilib.h
+++ src/include/efilib.h
@@ -1,14 +1,38 @@
-#ifndef EFILIB_H
-#define EFILIB_H
+#ifndef _EFI_LIB_H_
+#define _EFI_LIB_H_
 
-#include <efi.h>
-#include <efidef.h>
-#include <efiprot.h>
-#include <eficon.h>
+#include "efi.h"
+#include "eficon.h"
 
-// Basic helper functions used by many tutorials
-static inline void Print(EFI_SYSTEM_TABLE *SystemTable, CHAR16 *str) {
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, str);
+/*
+ * Simple internal helpers
+ */
+
+static inline UINTN StrLen(CHAR16 *Str) {
+    UINTN n = 0;
+    while (Str[n]) n++;
+    return n;
+}
+
+static inline EFI_STATUS Print(CHAR16 *String) {
+    return ST->ConOut->OutputString(ST->ConOut, String);
+}
+
+static inline EFI_STATUS ClearScreen() {
+    return ST->ConOut->ClearScreen(ST->ConOut);
+}
+
+static inline void *MemSet(void *buf, UINT8 value, UINTN size) {
+    UINT8 *p = buf;
+    while (size--) *p++ = value;
+    return buf;
+}
+
+static inline void *MemCopy(void *dst, const void *src, UINTN size) {
+    UINT8 *d = dst;
+    const UINT8 *s = src;
+    while (size--) *d++ = *s++;
+    return dst;
 }
 
 #endif

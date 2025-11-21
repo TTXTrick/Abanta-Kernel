a41168af2f8aa8b3cc4e2643d04af43f7a06eb28
TTXTrick <ttxtrick@gmail.com>
2025-11-21 07:30:27 -0500

    Create elf.h

---
+++ src/include/elf.h
@@ -0,0 +1,170 @@
+#ifndef ELF_H
+#define ELF_H
+
+#include <stdint.h>
+
+/* ---- ELF Base Types ---- */
+typedef uint16_t Elf64_Half;
+typedef uint32_t Elf64_Word;
+typedef uint64_t Elf64_Xword;
+typedef uint64_t Elf64_Addr;
+typedef uint64_t Elf64_Off;
+
+/* ---- ELF Identification ---- */
+#define EI_NIDENT 16
+
+/* ---- ELF Header ---- */
+typedef struct {
+    unsigned char e_ident[EI_NIDENT];
+    Elf64_Half    e_type;
+    Elf64_Half    e_machine;
+    Elf64_Word    e_version;
+    Elf64_Addr    e_entry;
+    Elf64_Off     e_phoff;
+    Elf64_Off     e_shoff;
+    Elf64_Word    e_flags;
+    Elf64_Half    e_ehsize;
+    Elf64_Half    e_phentsize;
+    Elf64_Half    e_phnum;
+    Elf64_Half    e_shentsize;
+    Elf64_Half    e_shnum;
+    Elf64_Half    e_shstrndx;
+} Elf64_Ehdr;
+
+/* ELF Types */
+#define ET_NONE   0
+#define ET_REL    1
+#define ET_EXEC   2
+#define ET_DYN    3
+
+/* Machine Type */
+#define EM_X86_64 62
+
+/* Ident Indexes */
+#define EI_MAG0   0
+#define EI_MAG1   1
+#define EI_MAG2   2
+#define EI_MAG3   3
+#define EI_CLASS  4
+#define EI_DATA   5
+
+/* Magic Bytes */
+#define ELFMAG0 0x7f
+#define ELFMAG1 'E'
+#define ELFMAG2 'L'
+#define ELFMAG3 'F'
+
+/* Class */
+#define ELFCLASS64 2
+
+/* ---- Program Header ---- */
+typedef struct {
+    Elf64_Word p_type;
+    Elf64_Word p_flags;
+    Elf64_Off  p_offset;
+    Elf64_Addr p_vaddr;
+    Elf64_Addr p_paddr;
+    Elf64_Xword p_filesz;
+    Elf64_Xword p_memsz;
+    Elf64_Xword p_align;
+} Elf64_Phdr;
+
+/* Program Types */
+#define PT_NULL  0
+#define PT_LOAD  1
+#define PT_DYNAMIC 2
+#define PT_INTERP 3
+#define PT_NOTE  4
+#define PT_SHLIB 5
+#define PT_PHDR  6
+
+/* Segment Flags */
+#define PF_X 1
+#define PF_W 2
+#define PF_R 4
+
+/* ---- Section Header ---- */
+typedef struct {
+    Elf64_Word sh_name;
+    Elf64_Word sh_type;
+    Elf64_Xword sh_flags;
+    Elf64_Addr  sh_addr;
+    Elf64_Off   sh_offset;
+    Elf64_Xword sh_size;
+    Elf64_Word  sh_link;
+    Elf64_Word  sh_info;
+    Elf64_Xword sh_addralign;
+    Elf64_Xword sh_entsize;
+} Elf64_Shdr;
+
+/* Section Types */
+#define SHT_NULL     0
+#define SHT_PROGBITS 1
+#define SHT_SYMTAB   2
+#define SHT_STRTAB   3
+#define SHT_RELA     4
+#define SHT_HASH     5
+#define SHT_DYNAMIC  6
+#define SHT_NOTE     7
+#define SHT_NOBITS   8
+#define SHT_REL      9
+#define SHT_DYNSYM   11
+
+/* ---- Symbol ---- */
+typedef struct {
+    Elf64_Word    st_name;
+    unsigned char st_info;
+    unsigned char st_other;
+    Elf64_Half    st_shndx;
+    Elf64_Addr    st_value;
+    Elf64_Xword   st_size;
+} Elf64_Sym;
+
+/* Symbol Binding */
+#define ELF64_ST_BIND(i)   ((i)>>4)
+#define ELF64_ST_TYPE(i)   ((i)&0xf)
+
+/* Symbol Bindings */
+#define STB_LOCAL  0
+#define STB_GLOBAL 1
+#define STB_WEAK   2
+
+/* Symbol Types */
+#define STT_NOTYPE  0
+#define STT_OBJECT  1
+#define STT_FUNC    2
+#define STT_SECTION 3
+#define STT_FILE    4
+
+/* ---- Relocation (with addend) ---- */
+typedef struct {
+    Elf64_Addr  r_offset;
+    Elf64_Xword r_info;
+    Elf64_Xword r_addend;
+} Elf64_Rela;
+
+#define ELF64_R_SYM(i)    ((i)>>32)
+#define ELF64_R_TYPE(i)   ((uint32_t)(i))
+
+/* Common x86_64 relocation types */
+#define R_X86_64_NONE       0
+#define R_X86_64_64         1
+#define R_X86_64_PC32       2
+#define R_X86_64_GOT32      3
+#define R_X86_64_PLT32      4
+#define R_X86_64_COPY       5
+#define R_X86_64_GLOB_DAT   6
+#define R_X86_64_JUMP_SLOT  7
+#define R_X86_64_RELATIVE   8
+#define R_X86_64_GOTPCREL   9
+#define R_X86_64_32         10
+#define R_X86_64_32S        11
+#define R_X86_64_16         12
+#define R_X86_64_8          13
+
+/* TLS relocations (needed later) */
+#define R_X86_64_DTPMOD64  16
+#define R_X86_64_DTPOFF64  17
+#define R_X86_64_TPOFF64   18
+
+#endif

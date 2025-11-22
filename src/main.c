bb3bb76df38b19fefb2fecebc2d5976549559fdd
TTXTrick <ttxtrick@gmail.com>
2025-11-22 12:58:36 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,103 +1,39 @@
-#include <efi.h>
-#include <efilib.h>
+TARGET = abanta
+SRC = src/main.c
 
-EFI_STATUS
-efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    EFI_STATUS Status;
-    EFI_LOADED_IMAGE *LoadedImage;
-    EFI_FILE_IO_INTERFACE *FileSystem;
-    EFI_FILE_HANDLE Volume;
-    EFI_FILE_HANDLE File;
-    UINTN FileSize;
-    UINT8 *Buffer;
+EFIINC = /usr/include/efi
+EFILIB = /usr/lib
 
-    InitializeLib(ImageHandle, SystemTable);
+CFLAGS = -I$(EFIINC) \
+         -I$(EFIINC)/protocol \
+         -fshort-wchar \
+         -mno-red-zone \
+         -fno-stack-protector \
+         -fpic \
+         -Wall -Wextra -O2
 
-    Print(L"[Abanta] Bootloader started\n");
+LDFLAGS = -T $(EFILIB)/elf_x86_64_efi.lds \
+          -shared \
+          -Bsymbolic \
+          -nostdlib \
+          $(EFILIB)/crt0-efi-x86_64.o \
+          -L$(EFILIB) \
+          -lefi -lgnuefi
 
-    //
-    // Get the LoadedImage protocol
-    //
-    Status = uefi_call_wrapper(
-        BS->HandleProtocol,
-        3,
-        ImageHandle,
-        &LoadedImageProtocol,
-        (void **)&LoadedImage
-    );
+all: build/$(TARGET).efi
 
-    if (EFI_ERROR(Status)) {
-        Print(L"Failed to get LoadedImage protocol: %r\n", Status);
-        return Status;
-    }
+build/$(TARGET).o: $(SRC)
+	mkdir -p build
+	gcc $(CFLAGS) -c $(SRC) -o build/$(TARGET).o
 
-    //
-    // Get SimpleFileSystem from device handle
-    //
-    Status = uefi_call_wrapper(
-        BS->HandleProtocol,
-        3,
-        LoadedImage->DeviceHandle,
-        &FileSystemProtocol,
-        (void **)&FileSystem
-    );
+build/$(TARGET).so: build/$(TARGET).o
+	ld $(LDFLAGS) build/$(TARGET).o -o build/$(TARGET).so
 
-    if (EFI_ERROR(Status)) {
-        Print(L"Failed to get FileSystem protocol: %r\n", Status);
-        return Status;
-    }
+build/$(TARGET).efi: build/$(TARGET).so
+	objcopy -j .text -j .sdata -j .data -j .dynamic \
+	        -j .dynsym -j .rel -j .rela -j .rel.* -j .rela.* \
+	        --target=efi-app-x86_64 \
+	        build/$(TARGET).so build/$(TARGET).efi
 
-    //
-    // Open root volume
-    //
-    Status = uefi_call_wrapper(FileSystem->OpenVolume, 2, FileSystem, &Volume);
-
-    if (EFI_ERROR(Status)) {
-        Print(L"Failed to open volume: %r\n", Status);
-        return Status;
-    }
-
-    //
-    // Open kernel file
-    //
-    Status = uefi_call_wrapper(
-        Volume->Open,
-        5,
-        Volume,
-        &File,
-        L"kernel.bin",
-        EFI_FILE_MODE_READ,
-        0
-    );
-
-    if (EFI_ERROR(Status)) {
-        Print(L"kernel.bin not found: %r\n", Status);
-        return Status;
-    }
-
-    //
-    // Get file size
-    //
-    EFI_FILE_INFO *FileInfo;
-    UINTN InfoSize = SIZE_OF_EFI_FILE_INFO + 200;
-
-    Status = uefi_call_wrapper(
-        BS->AllocatePool,
-        3,
-        EfiLoaderData,
-        InfoSize,
-        (void **)&FileInfo
-    );
-
-    Status = uefi_call_wrapper(File->GetInfo, 4,
-                               File,
-                               &FileInfoGuid,
-                               &InfoSize,
-                               FileInfo);
-
-    FileSize = FileInfo->FileSize;
-
-    Print(L"kernel.bin size = %d bytes\n", FileSize);
-
-    return EFI_SUCCESS;
-}
+clean:
+	rm -rf build
